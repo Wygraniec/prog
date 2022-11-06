e96549c96d0daf83fdd9a63d193af43fa7e0b1c8
Wygraniec <72353818+Wygraniec@users.noreply.github.com>
2022-11-06 20:10:09 +0100

    Update lekcja8.cpp

--- lekcja8.cpp
+++ lekcja8.cpp
@@ -1,84 +1,60 @@
 #include <stdio.h>
-
 #define SIZE 4
 
-using namespace std;
-
-void zapelnijTablice(float* tab, rozmiar) {
-    for (int i = 0; i < rozmiar; i++) {
-            for (int j = 0; j < rozmiar; j++) {
-                printf("Podaj liczbe o indeksie %i, %i: ", i, j);
-                scanf_s("%f", &tab[i][j]);
-            }
-        }
-}
-
-void wypiszTablice(float* tab, unsigned int rozmiar){
-    for (int i = 0; i < rozmiar; i++) {
-        for (int j = 0; j < rozmiar; j++) {
+void wypiszTablice(float(&tab)[SIZE][SIZE]) {
+    printf("\n");
+    for (unsigned int i = 0; i < SIZE; i++) {
+        for (unsigned int j = 0; j < SIZE; j++)
             printf("%8.2f", tab[i][j]);
-        }
         printf("\n");
     }
+    printf("\n");
 }
 
 int main() {
-    float liczby[SIZE][SIZE];
-    zapelnijTablice(liczby, SIZE);
+    float liczby[SIZE][SIZE] = {0};
     
-    wypiszTablice(liczby, SIZE);
+    for (unsigned int i = 0; i < SIZE; i++) {
+        for (unsigned int j = 0; j < SIZE; j++) {
+            printf("Podaj liczbe o indeksie %i, %i: ", i, j);
+            scanf_s("%f", &liczby[i][j]);
+        }
+    }
 
-    int valMin = liczby[0][0];
-    int valMaks = liczby[0][0];
-    unsigned int ileNajmniejszych = 0, ileNajwiekszych = 0;
-    unsigned int indeksyMin[SIZE][2] = {0};
-    unsigned int indeksyMaks[SIZE][2] = {0};
+    wypiszTablice(liczby);
+
+    float valMin = liczby[0][0];
+    float valMaks = liczby[0][0];
 
     //Znajdowanie najwiekszej i najmniejszej wartosci
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (valMin > liczby[i][j]) {
+            if (valMin > liczby[i][j]) 
                 valMin = liczby[i][j];
-                iMin = i;
-                jMin = j;
-            }
-
-            if (valMaks < liczby[i][j]) {
+            
+            if (valMaks < liczby[i][j])
                 valMaks = liczby[i][j];
-                iMaks = i;
-                jMaks = j;
-            }
+            
         }
     }
+    
+    printf("Najmniejsza wartosc to %2.2f\nNajwieksza wartosc to %2.2f\n", valMin, valMaks);
 
-    //Znajdowanie indeksow i ilosci najmniejszych i najwiekszych wartosci
-    for (int i = 0; i < SIZE; i++) {
+    //Zamiana wartosci najwiekszych z najmniejszymi i vice versa
+    for (int i = 0; i < SIZE; i++) 
         for (int j = 0; j < SIZE; j++) {
             if (liczby[i][j] == valMin) {
-                indeksyMin[ileNajmniejszych][0] = i;
-                indeksyMin[ileNajmniejszych][1] = j;
-                ileNajmniejszych++;              
+                liczby[i][j] = valMaks;
+                continue;
             }
 
             if (liczby[i][j] == valMaks) {
-                indeksyMin[ileNajwiekszych][0] = i;
-                indeksyMin[ileNajwiekszych][1] = j;
-                ileNajwiekszych++;
+                liczby[i][j] = valMin;
+                continue;
             }
         }
-    }
     
-    //Zamiana wartosci najmniejszych na najwieksze
-    for (int i = 0; i < ileNajmniejszych; i++) {
-        liczby[indeksyMin[i][0]][inedksyMin[i][1]] = valMaks;
-    }
+    wypiszTablice(liczby);
 
-    //Zamiana wartosci najwiekszych na najmniejsze
-    for (int i = 0; i < ileNajmniejszych; i++) {
-        liczby[indeksyMaks[i][0]][inedksyMaks[i][1]] = valMin;
-    }
-
-    wypiszTablice(liczby, SIZE);
-    
     return 0;
 }
