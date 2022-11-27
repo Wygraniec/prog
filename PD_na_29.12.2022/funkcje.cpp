8d932573145b9d1df016cd388367cf1aa145b55c
Wygraniec <72353818+Wygraniec@users.noreply.github.com>
2022-11-27 20:28:47 +0100

    Create funkcje.cpp

---
+++ PD_na_29.12.2022/funkcje.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "funkcje.h"
+
+void wypiszTablice(float* tab, int size) {
+    printf("\n");
+    for (int i = 0; i < size; i++)
+        printf(" %5.2f ", *(tab + i));
+    printf("\n");
+}
+
+float znajdzValMax(float* tab, int size) {
+    float valMax = *tab;
+
+    for (int i = 0; i < size; i++)
+        if (valMax < *(tab + i))
+            valMax = *(tab + i);
+
+#ifdef DEBUG
+    printf("valMax w funkcji: %f\n", valMax);
+#endif
+
+    return valMax;
+}
+
+float znajdzValMin(float* tab, int size) {
+    float valMin = *tab;
+
+    for (int i = 0; i < size; i++)
+        if (valMin > *(tab + i))
+            valMin = *(tab + i);
+
+#ifdef DEBUG
+    printf("valMin w funkcji: %f\n", valMin);
+#endif
+    return valMin;
+}
+
+void wypelnijTablice(float* tab, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("Podaj element o indeksie %i: ", i);
+        scanf_s("%f", tab + i);
+    }
+}
+
+void zamienNajwiekszeNajmniejsze(float* tab, int size, float valMin, float valMax) {
+    for (int i = 0; i < size; i++) {
+        if (*(tab + i) == valMax) {
+            *(tab + i) = valMin;
+            continue;
+        }
+
+        if (*(tab + i) == valMin) {
+            *(tab + i) = valMax;
+            continue;
+        }
+    }
+
+    printf("\nTablica po zamianie:");
+    wypiszTablice(tab, size);
+}
