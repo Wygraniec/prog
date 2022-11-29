0f929ad32e08627804685cf0887d22cdb30d8ed7
Wygraniec <72353818+Wygraniec@users.noreply.github.com>
2022-11-29 13:40:47 +0100

    Create funkcje.h

---
+++ lekcja11+PD/funkcje.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void wypiszTablice(float* tab, int size);
+
+float znajdzValMax(float* tab, int size);
+
+float znajdzValMin(float* tab, int size);
+
+void wypelnijTablice(float* tab, int size);
+
+void zamienNajwiekszeNajmniejsze(float* tab, int size, float valMin, float valMax);
+
+void sortuj(float* tab, int size);
+
+float mediana(float* tab, const int size);
