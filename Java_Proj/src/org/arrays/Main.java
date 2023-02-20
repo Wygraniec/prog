791eacad5568534adf2db2457bb6ad3d48e81af9
Wygraniec <wojciech.legocki@gmail.com>
2023-02-20 23:08:04 +0100

    fixes

--- Java_Proj/src/org/arrays/Main.java
+++ Java_Proj/src/org/arrays/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static Boolean debug = false;
     public static void printMenu() {
+        System.out.println();
         System.out.println("Obsługa tablicy 2D");
         System.out.println("0. Zakończ działanie programu");
         System.out.println("1. Wypisz tablicę");
@@ -12,6 +13,7 @@ public class Main {
         System.out.println("5. Wyświetl największą wartość w tablicy");
         System.out.println("6. Wyświetl średnią liczb w tablicy");
         System.out.println("7. Wyświetl medianę liczb w tablicy");
+        System.out.println();
     }
     public static void main(String[] args) {
 
@@ -34,58 +36,69 @@ public class Main {
             System.out.println("Średnia liczb w tablicy: " + arr.average());
             System.out.println("Mediana liczb w tablicy: " + arr.median());
         } else {
-            printMenu();
-            int selection = stdin.nextInt();
-            int x, y;
+            while (true) {
+                printMenu();
+                System.out.print("Wybór: ");
+                int selection = stdin.nextInt();
 
-            switch(selection) {
-                case 0:
-                    System.exit(0);
-                    break;
+                int x, y;
 
-                case 1:
-                    arr.print();
-                    break;
-
-                case 2:
-                    System.out.print("Podaj indeks x elementu: ");
-                    x = stdin.nextInt();
-
-                    System.out.print("Podaj indeks y elementu: ");
-                    y = stdin.nextInt();
-
-                    if(x < 0 || x >= arr.getSizeX() || y < 0 || y >= arr.getSizeY()){
-                        System.out.println("Podano niepoprawny indeks elementu");
+                switch (selection) {
+                    case 0:
+                        System.exit(0);
                         break;
-                    }
 
-                    else System.out.println("Wartość na wpisanym indeksie to " + arr.get(x, y));
-
-                case 3:
-                    System.out.print("Podaj indeks x elementu: ");
-                    x = stdin.nextInt();
-
-                    System.out.print("Podaj indeks y elementu: ");
-                    y = stdin.nextInt();
-
-                    if(x < 0 || x >= arr.getSizeX() || y < 0 || y >= arr.getSizeY()){
-                        System.out.println("Podano niepoprawny indeks elementu");
+                    case 1:
+                        arr.print();
                         break;
-                    }
 
-                    System.out.println("Podaj nową wartość elementu: ");
-                    int val = stdin.nextInt();
-                    arr.setValue(x, y, val);
-                    break;
+                    case 2:
+                        System.out.print("Podaj indeks x elementu: ");
+                        x = stdin.nextInt();
 
-                case 4:  System.out.println("Najmniejsza wartość: " + arr.minVal()); break;
+                        System.out.print("Podaj indeks y elementu: ");
+                        y = stdin.nextInt();
 
-                case 5:  System.out.println("Największa wartość: " + arr.maxVal()); break;
+                        if (x < 0 || x >= arr.getSizeX() || y < 0 || y >= arr.getSizeY()) {
+                            System.out.println("Podano niepoprawny indeks elementu");
+                            break;
+                        } else System.out.println("Wartość na wpisanym indeksie to " + arr.get(x, y));
+                        break;
 
-                case 6:System.out.println("Średnia liczb w tablicy: " + arr.average()); break;
+                    case 3:
+                        System.out.print("Podaj indeks x elementu: ");
+                        x = stdin.nextInt();
 
-                case 7: System.out.println("Mediana liczb w tablicy: " + arr.median()); break;
+                        System.out.print("Podaj indeks y elementu: ");
+                        y = stdin.nextInt();
 
+                        if (x < 0 || x >= arr.getSizeX() || y < 0 || y >= arr.getSizeY()) {
+                            System.out.println("Podano niepoprawny indeks elementu");
+                            break;
+                        }
+
+                        System.out.println("Podaj nową wartość elementu: ");
+                        int val = stdin.nextInt();
+                        arr.setValue(x, y, val);
+                        break;
+
+                    case 4:
+                        System.out.println("Najmniejsza wartość: " + arr.minVal());
+                        break;
+
+                    case 5:
+                        System.out.println("Największa wartość: " + arr.maxVal());
+                        break;
+
+                    case 6:
+                        System.out.println("Średnia liczb w tablicy: " + arr.average());
+                        break;
+
+                    case 7:
+                        System.out.println("Mediana liczb w tablicy: " + arr.median());
+                        break;
+
+                }
             }
         }
     }
